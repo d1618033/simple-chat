b693b1f238bb5bbb848b14a47baa628041ac14bc
d1618033 <d1618033@gmail.com>
2015-03-09 21:15:58 +0200

    add room detail and room list views

--- views.py
+++ views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from simplechat.models import Room
+from rest_framework.generics import ListCreateAPIView
+from simplechat.serializers import RoomSerializer
 
 
 class Index(TemplateView):
     template_name = "simplechat/index.html"
+
+
+class RoomList(ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class RoomView(DetailView):
+    queryset = Room.objects.all()
+    template_name = "simplechat/room.html"
 
 
