1a20ec5f393b0966b6102f769759399329ebf5a5
d1618033 <d1618033@gmail.com>
2015-03-09 20:04:26 +0200

    add basic models for room based chat

--- models.py
+++ models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    pass
+
+
+class Participant(models.Model):
+    room = models.ForeignKey(Room)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room)
+    participant = models.ForeignKey(Participant)
+    message = models.TextField()
+
+    def __str__(self):
+        return "[{0}]: {1}".format(self.participant, self.message)
