b4764f2f18b2d0a2ee354ebeb4e06f2f7b932620
d1618033 <d1618033@gmail.com>
2015-03-09 21:15:23 +0200

    add room list view and room detail view in urls

--- urls.py
+++ urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from simplechat import views
 
 urlpatterns = patterns("",
-    url("^$", views.Index.as_view(), name="index"),
+    url(r"^$", views.Index.as_view(), name="index"),
+    url(r"^api/room/", views.RoomList.as_view(), name="room_list"),
+    url(r"^room/(?P<pk>\d+)", views.RoomView.as_view(), name="room_detail"),
 )
