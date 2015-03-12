cad45a21db4b8e5f49951ef82d97b9065aa79c07
d1618033 <d1618033@gmail.com>
2015-03-12 22:07:29 +0200

    remove api urls from simple chat

--- simplechat/urls.py
+++ simplechat/urls.py
@@ -3,8 +3,5 @@ from simplechat import views
 
 urlpatterns = patterns("",
     url(r"^$$", views.Index.as_view(), name="index"),
-    url(r"^api/room/$", views.RoomList.as_view(), name="room_list"),
     url(r"^room/(?P<pk>\d+)/$", views.RoomView.as_view(), name="room_detail"),
-    url(r"^api/participant/$", views.ParticipantList.as_view(), name="participant_list"),
-    url(r"^api/participant/(?P<pk>\d+)/$", views.ParticipantDetail.as_view(), name="participant_detail"),
 )
