8e1a969d48bdd4dfd49e49d1dccbf8b606133bc0
d1618033 <d1618033@gmail.com>
2015-03-12 21:51:19 +0200

    move api views to api folder

--- simplechat/views.py
+++ simplechat/views.py
@@ -1,28 +1,11 @@
 from django.views.generic import TemplateView, DetailView
 from simplechat.models import Room, Participant
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from simplechat.serializers import RoomSerializer, ParticipantSerializer
 
 
 class Index(TemplateView):
     template_name = "simplechat/index.html"
 
 
-class RoomList(ListCreateAPIView):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
-
-
 class RoomView(DetailView):
     queryset = Room.objects.all()
     template_name = "simplechat/room.html"
-
-
-class ParticipantList(ListCreateAPIView):
-    queryset = Participant.objects.all()
-    serializer_class = ParticipantSerializer
-
-
-class ParticipantDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Participant.objects.all()
-    serializer_class = ParticipantSerializer
--- simplechat_api/views.py
+++ simplechat_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from simplechat.models import Room, Participant
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from simplechat_api.serializers import RoomSerializer, ParticipantSerializer
 
-# Create your views here.
+
+class RoomList(ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ParticipantList(ListCreateAPIView):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+
+
+class ParticipantDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
