f970cc0153a510396604c55ba73808cfd5f72973
d1618033 <d1618033@gmail.com>
2015-03-16 19:57:55 +0200

    add functional tests

--- simplechat_api/tests.py
+++ simplechat_api/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.core.urlresolvers import reverse
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
-# Create your tests here.
+
+class SeleniumTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        firefox_binary = FirefoxBinary('/usr/bin/firefox')
+        firefox_profile = FirefoxProfile("/home/david/.mozilla/firefox/btz4i8jg.Default User/")
+        cls.selenium = webdriver.Firefox(firefox_binary=firefox_binary, firefox_profile=firefox_profile)
+        super(SeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(SeleniumTests, cls).tearDownClass()
+
+    def get(self, url):
+        return self.selenium.get("{0}{1}".format(self.live_server_url, url))
+
+    def get_text_body(self):
+        return self.selenium.find_element_by_tag_name("body").text
+
+
+class TestChat(SeleniumTests):
+    def test_index(self):
+        self.get(reverse("simplechat:index"))
+        self.assertIn("Create a new room", self.get_text_body())
