84d31cd0b9e449fae169778a05a7e390d6752c26
d1618033 <d1618033@gmail.com>
2015-04-02 07:33:36 +0300

    use permission class in message and not password mixin

--- simplechat_api/views.py
+++ simplechat_api/views.py
@@ -2,44 +2,15 @@ from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import PermissionDenied
 
 from simplechat.models import Room, Participant, Message
 from simplechat_api.serializers import RoomSerializer, ParticipantSerializer, MessageSerializer
-from simplechat_api.permisssion import CanEditParticipant
+from simplechat_api.permisssion import CanEditParticipant, CanEditMessage
 
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-
-
-class CheckPasswordMixin:
-    def create(self, request, *args, **kwargs):
-        if not self.check_password(request):
-            raise PermissionDenied
-        return super(CheckPasswordMixin, self).create(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        if not self.check_password(request):
-            raise PermissionDenied
-        return super(CheckPasswordMixin, self).update(request, *args, **kwargs)
-
-    def partial_update(self, request, *args, **kwargs):
-        if not self.check_password(request):
-            raise PermissionDenied
-        return super(CheckPasswordMixin, self).partial_update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        if not self.check_password(request):
-            raise PermissionDenied
-        return super(CheckPasswordMixin, self).destroy(request, *args, **kwargs)
-
-    def partial_destroy(self, request, *args, **kwargs):
-        if not self.check_password(request):
-            raise PermissionDenied
-        return super(CheckPasswordMixin, self).partial_destroy(request, *args, **kwargs)
 
 
 class ParticipantViewSet(viewsets.ModelViewSet):
@@ -48,16 +19,10 @@ class ParticipantViewSet(viewsets.ModelViewSet):
     permission_classes = (CanEditParticipant,)
 
 
-class MessageViewSet(CheckPasswordMixin, viewsets.ModelViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
-
-    def check_password(self, request):
-        participant_pk = request.data['participant']
-        participant = get_object_or_404(Participant, pk=participant_pk)
-        if participant.password != request.data.get('password', ''):
-            return False
-        return True
+    permission_classes = (CanEditMessage,)
 
     @list_route()
     def recent(self, request):
